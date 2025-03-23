f4281e1a444df956b2fc40834f2271802438f5a2
Peris Wainaina <wainainaperis84@gmail.com>
2025-03-23 16:36:14 +0300

    First Inventory Commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,12 @@
+
+
+<div>
+    <form class="form">
+    <h2>RBN Inventory Management System</h2>
+    <label for="email">Email Address: </label>
+    <input type="text" name="email"/><br><br>
+    <label for="password">Password: </label>
+    <input type="password" name="password"/><br><br>
+    <button>Login</button>
+    </form>
+</div>
