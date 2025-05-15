9afdfce84bf05fe063d26700a5e708d53ff1fdbd
Peris Wainaina <wainainaperis84@gmail.com>
2025-05-15 20:23:19 +0300

    Profile Completed

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,6 @@
+@extends('dashboard')
+
+@section('content')
 @if (session('success'))
     <div style="color: green;">{{ session('success') }}</div>
 @endif
@@ -7,30 +10,51 @@
         @csrf
         <div>
             @if(auth()->user()->profile_picture)
-                <img src="{{ asset('storage/' . auth()->user()->profile_picture) }}" width="100" height="100"><br>
+                <img src="{{ asset('storage/' . auth()->user()->profile_picture) }}" width="100" height="100"><br><br>
             @endif
             <input type="file" name="profile_picture" required>
-            <button type="submit">Upload</button>
+            <button class="dt3" type="submit">Upload</button>
         </div>
     </form>
 </div>
 
 <div id="changePasswordForm" style="display:none;">
     <h3>Change Password</h3>
-    <form action="{{ route('profile.update.password') }}" method="POST">
+    <form class="dt2" action="{{ route('profile.update.password') }}" method="POST">
         @csrf
         <div>
             <label>Current Password:</label>
             <input type="password" name="current_password" required>
-        </div>
+        </div><br>
         <div>
             <label>New Password:</label>
             <input type="password" name="new_password" required>
-        </div>
+        </div><br>
         <div>
             <label>Confirm New Password:</label>
             <input type="password" name="new_password_confirmation" required>
-        </div>
+        </div><br>
         <button type="submit">Change Password</button>
     </form>
 </div>
+
+<script>
+    function handleHashChange() {
+        document.getElementById("changePictureForm").style.display = "none";
+        document.getElementById("changePasswordForm").style.display = "none";
+
+        const hash = window.location.hash;
+
+        if (hash === "#changePictureForm") {
+            document.getElementById("changePictureForm").style.display = "block";
+        }
+
+        if (hash === "#changePasswordForm") {
+            document.getElementById("changePasswordForm").style.display = "block";
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", handleHashChange);
+    window.addEventListener("hashchange", handleHashChange);
+</script>
+@endsection
